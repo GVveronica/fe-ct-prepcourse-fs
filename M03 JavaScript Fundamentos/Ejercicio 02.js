5f63961a8f5214258be5e407a4608d1a429e3bde
vero.g <vero.crzz@live.com.ar>
2023-06-01 10:45:28 -0300

    Modulos Prep

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -3,34 +3,40 @@
 
 function devolverString(string) {
    // Debe retornar un string.
-   // Tu código:
+   // Tu Código :
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
-   // Tu código:
+   // Tu Código:
+   return x + y ;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
-   // Tu código:
+   //  Tu Código:
+   return x - y;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
-   // Tu código:
+   // Tu Código:
+   return  x / y;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
-   // Tu código:
+   //  Tu Código:
+   return x * y;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+   //  Tu Código:
+   return x % y; 
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
